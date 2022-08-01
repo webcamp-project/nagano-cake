955b715a6fe20e7be353f21f39d45938951f6a68
MasaWebCamp <108528197+MasaWebCamp@users.noreply.github.com>
2022-08-01 11:55:59 +0000

    [Make]Admin及びPublicのコントローラーとビューファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :admins
+
+  #会員側のルーティング設定
+  namespace :public do
+    root to: 'homes#top'
+    get '/home/about' => 'homes#about'
+  end
+
+  devise_for :customers,skip: [:passwords], controllers: {
+  sessions: 'public/sessions',
+  registrations: 'public/registrations'
+  }
+
+  #管理側のルーティング設定
+  namespace :admin do
+    get "/" => "homes#top"
+  end
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
