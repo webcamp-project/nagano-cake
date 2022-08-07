3ab129a3cb4544ae0e19f53915e1841edc6a0c2b
MasaWebCamp <108528197+MasaWebCamp@users.noreply.github.com>
2022-08-07 10:45:16 +0000

    [Make]商品新規登録画、詳細画面、編集画面の作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "1e7d68fcd6974b009b1b08bf92f68a7e.vfs.cloud9.us-east-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
