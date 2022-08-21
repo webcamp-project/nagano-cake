e2750b49e4cc9d1693a3cc6a141fe75bba552038
MasaWebCamp <108528197+MasaWebCamp@users.noreply.github.com>
2022-08-21 11:14:59 +0000

    [Make]顧客用の注文履歴詳細の作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,29 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
-  
+
   def update
+    params[:order][:receive_status] = params[:order][:receive_status].to_i
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    if @order.update(order_params)
+    redirect_to admin_order_path(@order.id)
+    else
+    render :show
+    end
+
+    if @order.receive_status == "入金確認"
+      @order_details.update_all(making_status:1)
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:receive_status)
   end
 end
